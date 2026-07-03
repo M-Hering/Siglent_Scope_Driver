41f77bd0532adb99dd6819189e9eb6ca09f3ddb4
Hering <m_heri02@uni-muenster.de>
2026-07-03 12:53:59 +0200

    array_mean_to better parameter check

--- util/array_mean_to.m
+++ util/array_mean_to.m
@@ -1,4 +1,8 @@
 function meaned = array_mean_to(data, n)
   % Takes the averrage of so many points that n points remain
-  meaned = array_mean(data, floor(length(data)/n));
+  if n>0
+    meaned = array_mean(data, floor(length(data)/n));
+  else
+    meaned = data;
+  end
 end
